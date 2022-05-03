6e8add5cf4e221b77d915fe656e0ac1956e039fe
Mouad Benali <mouadbnl.2k01@gmail.com>
2022-05-03 17:31:50 +0000

    CRUD for categories & transaction

---
+++ api/app/Http/Resources/TransactionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'category_id' => $this->category_id,
+            'category_name' => $this->category->name,
+            'transaction_date' => $this->transaction_date->format('m/d/Y'),
+            'created_at' => $this->created_at,
+            'description' => $this->description,
+        ];
+    }
+}
---
+++ api/app/Models/Transaction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    protected $dates = ['transaction_date'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    protected function amount() : Attribute {
+        return Attribute::make(
+            get: fn ($v) => number_format($v / 100, 2),
+            set: fn ($v) => $v * 100
+        );
+    }
+
+    protected function transactionDate() : Attribute {
+        return Attribute::make(
+            set: fn ($v) => Carbon::createFromFormat('d/m/Y', $v)->format('Y-m-d')
+        );
+    }
+
+}
